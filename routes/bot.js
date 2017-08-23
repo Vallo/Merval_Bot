d28578b3bc34447103ecfec56a26fc9f79dc37ad
Unknown <vallo61@gmail.com>
2017-08-23 14:26:54 -0300

    test1

--- routes/bot.js
+++ routes/bot.js
@@ -9,7 +9,6 @@ router.post('/' + config.token, function(req, res, next) {
 	try {
 	    bot.processUpdate(req.body);
 		res.sendStatus(200);
-		console.log(req.body);
 	} 
 	catch (err) {
     // handle the error safely
@@ -17,8 +16,17 @@ router.post('/' + config.token, function(req, res, next) {
 	}
 });
 
-router.get('/test', function(req, res, next) {
-	res.send('ok');
-	console.log('ok');
+// Matches /love
+bot.onText(/\/love/, function onLoveText(msg) {
+  const opts = {
+    reply_to_message_id: msg.message_id,
+    reply_markup: JSON.stringify({
+      keyboard: [
+        ['Yes, you are the bot of my life ❤'],
+        ['No, sorry there is another one...']
+      ]
+    })
+  };
+  bot.sendMessage(msg.chat.id, 'Do you love me?', opts);
 });
 module.exports = router;
