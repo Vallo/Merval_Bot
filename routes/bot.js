8f99d02e4689bcf6cd1b46bf7c94451e09e35f58
Unknown <vallo61@gmail.com>
2017-08-23 15:44:31 -0300

    se

--- routes/bot.js
+++ routes/bot.js
@@ -16,26 +16,11 @@ router.post('/' + config.token, function(req, res, next) {
 	}
 });
 
-// Matches /love
-bot.onText(/\/love/, function onLoveText(msg) {
-  const opts = {
-    reply_to_message_id: msg.message_id,
-    reply_markup: JSON.stringify({
-      keyboard: [
-        ['Yes, you are the bot of my life ❤'],
-        ['No, sorry there is another one...']
-      ]
-    })
-  };
-  bot.sendMessage(msg.chat.id, 'Do you love me?', opts);
-});
-
 bot.onText(/\/start/, function onStartText(msg) {
   const opts = {
-    reply_to_message_id: msg.message_id,
     reply_markup: {"force_reply": true}
   };
-  bot.sendMessage(msg.chat.id, 'Hola!', opts).then(function(sended){
+  bot.sendMessage(msg.chat.id, 'Hola!' + msg.from.first_name, opts).then(function(sended){
   	var chatId = sended.chat.id;
     var messageId = sended.message_id;
     bot.onReplyToMessage(chatId, messageId, function(message) {
@@ -43,26 +28,6 @@ bot.onText(/\/start/, function onStartText(msg) {
     });
   });
 });
-
-
-bot.onText(/\/editable/, function onEditableText(msg) {
-  const opts = {
-    reply_markup: {
-      inline_keyboard: [
-        [
-          {
-            text: 'Edit Text',
-             // we shall check for this value when we listen
-             // for "callback_query"
-            callback_data: 'edit'
-          }
-        ]
-      ]
-    }
-  };
-  bot.sendMessage(msg.from.id, 'Original Text', opts);
-});
-
 
 // Handle callback queries
 bot.on('callback_query', function onCallbackQuery(callbackQuery) {
