bb3c81685cf40925db66ac5a7a00788172bc1d46
Unknown <vallo61@gmail.com>
2017-08-23 14:07:41 -0300

    e

--- app.js
+++ app.js
@@ -12,8 +12,8 @@ var bodyParser = require('body-parser');
 var botRoute = require('./routes/bot');
 
 //init bot
-const bot = new TelegramBot(TOKEN);
-bot.setWebHook(`${url}/bot/${TOKEN}`);
+//const bot = new TelegramBot(TOKEN);
+//bot.setWebHook(`${url}/bot/${TOKEN}`);
 
 
 var app = express();
