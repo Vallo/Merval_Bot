ae64e27d173fd030bb4179e03305940088f8874f
Unknown <vallo61@gmail.com>
2017-08-23 11:50:03 -0300

    Auto stash before merge of "master" and "origin/master"

--- routes/bot.js
+++ routes/bot.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 var config = require('../config');
+var http = require('http');
 /* GET users listing. */
 router.post('/' + config.bot, function(req, res, next) {
   res.sendStatus(200);
+  
   console.log(req.body)
 });
 
