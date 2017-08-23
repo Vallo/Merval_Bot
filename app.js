96124cc46eef798b8a25cafa55cdabec4c8dc46f
Unknown <vallo61@gmail.com>
2017-08-23 12:21:46 -0300

    init

--- app.js
+++ app.js
@@ -1,4 +1,39 @@
+const config = require('./config');
+const TOKEN = config.token;
+const url = 'https://' + config.Ip;
+const port = 433;
 const TelegramBot = require('node-telegram-bot-api');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+// No need to pass any parameters as we will handle the updates with Express
+const bot = new TelegramBot(TOKEN);
+
+// This informs the Telegram servers of the new webhook.
+bot.setWebHook(`${url}/bot${TOKEN}`);
+
+const app = express();
+
+// parse the updates to JSON
+app.use(bodyParser.json());
+
+// We are receiving updates at the route below!
+app.post(`/bot${TOKEN}`, (req, res) => {
+  bot.processUpdate(req.body);
+  res.sendStatus(200);
+});
+
+// Start Express Server
+app.listen(port, () => {
+  console.log(`Express server is listening on ${port}`);
+});
+
+// Just to ping!
+bot.on('message', msg => {
+  bot.sendMessage(msg.chat.id, 'I am alive!');
+});
+
+/*
 const config = require('./config.js');
 
 const bot = new TelegramBot(config.bot);
@@ -26,4 +61,4 @@ bot.on('message', (msg) => {
 
   // send a message to the chat acknowledging receipt of their message
   bot.sendMessage(chatId, 'Received your message');
-});
+});*/
